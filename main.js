b21e861eb63e3fbcedee6201e9bcfd607f8a7f8e
arronrees <64908445+arronrees@users.noreply.github.com>
2023-07-08 17:56:48 +0100

    Added lenis scroll

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+// css
 import './styles/style.scss';
+
+// js
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function initScroll() {
+  const lenis = new Lenis({ smoothWheel: true, lerp: 0.05 });
+
+  lenis.on('scroll', ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initScroll();
+});
